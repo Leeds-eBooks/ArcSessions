d893e1b40eb5434aefc542f04f9a94fbe0478cc7
Ben Styles <ben@arcpublications.co.uk>
2015-01-15 16:26:36 +0000

    Added echo.js for lazy loading

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 
 {% include tap.min.js %}
 {% include rivets.bundled.min.js %}
+{% include echo.min.js %}
 
 window.S={
   videoArray: [
@@ -18,7 +19,7 @@ window.S={
     {% endfor %}
   ],
   init: function() {
-    var items=document.querySelectorAll('.post-list > li'),
+    var listItems=document.querySelectorAll('.post-list > li'),
         list=document.querySelector('.post-list'),
         itemsArray=[],
         vidEls=document.getElementsByClassName("youtube"),
@@ -42,10 +43,10 @@ window.S={
         return 0;
       });
       sortedEls=model.array.map(function(v) {return v.el;});
-      for (var i=0;i<items.length;i++) {
-        pos=sortedEls.indexOf(items[i]); // find the item's position in the sorted list
-        if (pos!==i) {
-          list.insertBefore(items[i],items[pos+1]); // TODO check
+      for (var i=0;i<listItems.length;i++) {
+        pos=sortedEls.indexOf(listItems[i]); // find the item's position in the sorted list
+        if (pos!==i) { // if the item is not in the right position
+          list.insertBefore(listItems[i],listItems[pos+1]); // TODO check
         }
       }
     }
@@ -60,11 +61,21 @@ window.S={
       }
     };
 
+    // init() -->
+    echo.init({
+      offset: 100,
+      throttle: 250,
+      unload: false,
+      callback: function (element, op) {
+        console.log(element, 'has been', op + 'ed')
+      }
+    });
+
     for (var i=0;i<vidEls.length;i++) {
       vidEls[i].addEventListener('click',loadYouTube,false);
     }
-    for (var j=0;j<items.length;j++) {
-      itemsArray.push({el:items[j],obj:S.videoArray[j]});
+    for (var j=0;j<listItems.length;j++) {
+      itemsArray.push({el:listItems[j],obj:S.videoArray[j]});
     }
 
     rivets.bind(document.querySelector('div.home'),model);
