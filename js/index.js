4e3c8e07790244f008cd7dc5b5310b82cc2776ea
Ben Styles <enquiries@leeds-ebooks.co.uk>
2016-11-07 15:43:12 +0000

    refactor(youtube): &amp; to &

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ window.S = {
 
       iframe.setAttribute(
         "src",
-        "//www.youtube-nocookie.com/embed/" + this.id + "?rel=0&amp;showinfo=0&amp;autoplay=1"
+        "//www.youtube-nocookie.com/embed/" + this.id + "?rel=0&showinfo=0&autoplay=1"
       )
 
       iframe.setAttribute("frameborder", "0")
