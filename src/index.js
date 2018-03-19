67395f22e9735d57fd4066d3716af13f17cdfed7
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-03-19 13:32:56 +0000

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+// @flow
+
 import './tap.min'
-import echo from 'echo-js'
+import 'echo-js'
 import rivets from 'rivets'
 import rapt from 'rapt'
-import videoArray from '../_data/videos.yml'
+import {maybe} from 'maybes'
+import videoArray, {type Video} from '../_data/videos.yml'
+
+// eslint-disable-next-line no-sequences
+const logAndReturn = x => (console.log(x), x)
+
+type Model = {
+  array: Array<{
+    el: HTMLElement,
+    obj: Video,
+  }>,
+  filter: () => void,
+  sort_poet?: (event: Event) => void,
+  sort_date?: (event: Event) => void,
+  sort_duration?: (event: Event) => void,
+}
+
+let list: HTMLElement
+
+const getVideo = (id: string) => {
+  const found = videoArray.find(video => video.id === id)
+  if (!found) throw new TypeError(`Could not find video for id ${id}`)
+  return found
+}
 
 function stylePrefix(element, property, value) {
   const prop = property.charAt(0).toUpperCase() + property.substr(1)
@@ -28,7 +53,7 @@ function loadYouTube() {
     height: this.style.height,
   })
   this.parentNode.replaceChild(iframe, this)
-  iframe.parentNode.style.opacity = '1'
+  maybe(iframe.parentNode).forEach(p => ((p: $Hack).style.opacity = '1'))
 }
 /* eslint-enable babel/no-invalid-this */
 
@@ -37,101 +62,111 @@ const convertToSecs = str =>
     .map(a => parseInt(a[0], 10) * 60 + parseInt(a[1], 10))
     .val()
 
-function sort(model, list, by, button, descending) {
+const sorter = (by, descending, a, b) => {
+  const placeBefore = descending ? -1 : 1
+  const placeAfter = descending ? 1 : -1
+
+  const A = a.obj
+  const B = b.obj
+
+  if (by === 'duration') {
+    const aSecs = convertToSecs(A.duration)
+    const bSecs = convertToSecs(B.duration)
+    if (aSecs > bSecs) return placeBefore
+    if (aSecs < bSecs) return placeAfter
+  } else {
+    if (A[by] > B[by]) return placeBefore
+    if (A[by] < B[by]) return placeAfter
+  }
+  return 0
+}
+
+const sort = (model: Model, by: string, button, descending) => {
   stylePrefix(
-    button.querySelector('.sort-arrow'),
+    (button: $Hack).querySelector('.sort-arrow'),
     'transform',
     descending ? 'rotate(180deg)' : null
   )
 
-  model.array.sort((a, b) => {
-    const sortFirst = descending ? -1 : 1
-    const sortSecond = descending ? 1 : -1
-    const A = a.obj
-    const B = b.obj
+  model.array.sort((a, b) => sorter(by, descending, a, b))
+  const sortedList = list.cloneNode(false)
 
-    if (by === 'duration') {
-      const aSecs = convertToSecs(A.duration)
-      const bSecs = convertToSecs(B.duration)
-      if (aSecs > bSecs) return sortFirst
-      if (aSecs < bSecs) return sortSecond
-    } else {
-      if (A[by] > B[by]) return sortFirst
-      if (A[by] < B[by]) return sortSecond
-    }
-    return 0
-  })
+  model.array
+    .map(v => {
+      console.log(v.el.textContent)
+      return v.el
+    })
+    .forEach(el => sortedList.appendChild(el))
 
-  const newUl = list.cloneNode(false)
+  maybe(list.parentNode).forEach(p => p.replaceChild(sortedList, list))
 
-  model.array.map(v => v.el).forEach(el => newUl.appendChild(el))
-
-  list.parentNode.replaceChild(newUl, list)
-  list = newUl
-  echo.render()
+  list = sortedList
+  window.echo.render()
 }
 
-function sortClosure(model, list, by) {
+const sortClosure = (model: Model, by: string) => {
   let descending = true
   return event => {
-    sort(by, event.currentTarget, descending)
+    sort(model, by, event.currentTarget, descending)
     descending = !descending
   }
 }
 
+const keysToIgnore = ['id', 'duration']
+const shouldIgnoreKey = (key: $Keys<Video>) => keysToIgnore.includes(key)
+
 const init = () => {
-  const listItems = document.querySelectorAll('.post-list > li')
-  const list = document.querySelector('.post-list')
-  const itemsArray = []
-  const vidEls = document.getElementsByClassName('youtube')
-  const model = {
-    array: itemsArray,
+  list = rapt(document.querySelector('.post-list'))
+    .map(l => {
+      if (l === null) throw new TypeError('.post-list matches 0 elements')
+      else return l
+    })
+    .val()
+
+  const listItems = Array.from(document.querySelectorAll('.post-list > li'))
+  const vidEls = Array.from(document.getElementsByClassName('youtube'))
+
+  const model: Model = {
+    array: [],
 
     filter() {
-      const q = this.value
+      const query = (this.value: string)
         .toLowerCase()
         .trim()
         .split(' ')
 
-      function check(el) {
-        const shouldShow = []
+      function check(video) {
+        const finder = v => v.el.id === video.id
 
-        function finder(v) {
-          return v.el.id === el.lastName
-        }
+        const shouldShow = query.every(token =>
+          Object.keys(video).reduce((val, key) => {
+            if (shouldIgnoreKey(key)) return val
 
-        for (let i = 0; i < q.length; i += 1) {
-          for (let key in el) {
-            if (key !== 'id') {
-              if (el[key].toLowerCase().indexOf(q[i]) > -1) {
-                shouldShow[i] = true
-              } else {
-                shouldShow[i] = shouldShow[i] || false
-              }
+            if (video[key].toLowerCase().includes(token)) {
+              return true
+            } else {
+              return val || false
             }
-          }
-        }
+          }, false)
+        )
 
-        if (shouldShow.length && shouldShow.every(Boolean)) {
-          itemsArray.find(finder).el.style.display = 'block'
-        } else {
-          itemsArray.find(finder).el.style.display = 'none'
-        }
+        maybe(model.array.find(finder)).forEach(({el}) => {
+          el.style.display = shouldShow ? 'block' : 'none'
+        })
       }
 
       videoArray.forEach(check)
-
-      echo.render()
+      window.echo.render()
     },
   }
 
   Object.assign(model, {
-    sort_poet: sortClosure(model, list, 'lastName'),
-    sort_date: sortClosure(model, list, 'date'),
-    sort_duration: sortClosure(model, list, 'duration'),
+    sort_poet: sortClosure(model, 'lastName'),
+    sort_date: sortClosure(model, 'date'),
+    sort_duration: sortClosure(model, 'duration'),
   })
 
-  echo.init({
+  window.echo.init({
     offset: 500,
     throttle: 250,
     unload: false,
@@ -142,13 +177,9 @@ const init = () => {
 
   vidEls.forEach(el => el.addEventListener('click', loadYouTube, false))
 
-  itemsArray.push(
-    ...listItems.map((item, i) => ({
-      el: item,
-      obj: videoArray[i],
-    }))
-  )
+  model.array.push(...listItems.map(el => ({el, obj: getVideo(el.id)})))
 
+  // eslint-disable-next-line no-restricted-syntax
   rivets.bind(document.querySelector('div.home'), model)
 }
 
